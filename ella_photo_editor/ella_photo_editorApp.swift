87e8a6188de4f05f822508d0b0299cefb387e5bd
Galen Ostroot <zdw1224@gmail.com>
2024-12-03 22:25:06 +0800

    Initial Commit

---
+++ ella_photo_editor/ella_photo_editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ella_photo_editorApp.swift
+//  ella_photo_editor
+//
+//  Created by Zhan Wei wei on 2024/12/3.
+//
+
+import SwiftUI
+
+@main
+struct ella_photo_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ella_photo_editorTests/ella_photo_editorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ella_photo_editorTests.swift
+//  ella_photo_editorTests
+//
+//  Created by Zhan Wei wei on 2024/12/3.
+//
+
+import Testing
+@testable import ella_photo_editor
+
+struct ella_photo_editorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
